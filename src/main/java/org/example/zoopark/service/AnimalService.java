77a3a7cd807977fb6f507593f95e7a567faf8443
Akybek Abdikulov <akybek.abdikulov@narxoz.kz>
2025-12-18 17:04:46 +0500

    I have changed servises and completed needed comands

--- src/main/java/org/example/zoopark/service/AnimalService.java
+++ src/main/java/org/example/zoopark/service/AnimalService.java
@@ -1,14 +1,14 @@
 package org.example.zoopark.service;
 
+import org.example.zoopark.dto.AnimalDto;
 import org.example.zoopark.entity.Animal;
 
 import java.util.List;
 
 public interface AnimalService {
-    Animal save(Animal animal);
-
-    List<Animal> findAll();
-
-    Animal findById(Long id);
-    void deleteById(Long id);
+    AnimalDto create(AnimalDto animalDto);
+    List<AnimalDto> getAll();
+    AnimalDto getById(Long id);
+    AnimalDto update(Long id, AnimalDto animalDto);
+    void delete(Long id);
 }
