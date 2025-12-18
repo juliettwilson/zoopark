e95807bc6ff6d33214e01b6d65a3341183058ba3
AIZHAN <154900195+juliettwilson@users.noreply.github.com>
2025-12-18 17:33:41 +0500

    created repository enclosure
    * created 2 repositories are called Animal and Keeper by Aizhan
    
    * created repository enclosure
    
    ---------
    
    Co-authored-by: Aizhan <aizhanzhs@icloud.com>

---
+++ src/main/java/org/example/zoopark/repository/EnclosureRepository.java
@@ -0,0 +1,7 @@
+package org.example.zoopark.repository;
+
+import org.example.zoopark.entity.Enclosure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnclosureRepository extends JpaRepository<Enclosure, Long> {
+}
