119eee8f209d10fa46671869efaf1bcd9fa67282
Aizhan <aizhanzhs@icloud.com>
2025-12-18 14:40:01 +0500

    I created 2 entitys by Aizhan

---
+++ src/main/java/org/example/zoopark/entity/Animal.java
@@ -0,0 +1,27 @@
+package org.example.zoopark.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "animal_name")
+    private String name;
+    @Column(name = "animal_species")
+    private String species;
+    @Column(name = "animal_age")
+    private int age;
+
+    @ManyToMany
+    private Set<Keeper> keepers = new HashSet<>();
+}
