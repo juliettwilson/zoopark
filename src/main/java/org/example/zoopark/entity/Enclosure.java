da9810a1f146f5b197fd8e0f212b9c7edb85ffda
Akybek Abdikulov <akybek.abdikulov@narxoz.kz>
2025-12-18 17:16:53 +0500

    I have changed enclosure entity

--- src/main/java/org/example/zoopark/entity/Enclosure.java
+++ src/main/java/org/example/zoopark/entity/Enclosure.java
@@ -1,9 +1,14 @@
 package org.example.zoopark.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "enclosures")
 public class Enclosure {
 
@@ -19,44 +24,4 @@ public class Enclosure {
 
     @OneToMany(mappedBy = "enclosure")
     private List<Animal> animals;
-
-    public Enclosure() {
-    }
-
-    public Enclosure(String name, String type) {
-        this.name = name;
-        this.type = type;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public List<Animal> getAnimals() {
-        return animals;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setAnimals(List<Animal> animals) {
-        this.animals = animals;
-    }
 }
