72caabe4c227c775de27f8fa841dcc36e3106887
Aizhan <aizhanzhs@icloud.com>
2025-12-18 14:34:23 +0500

    my base project

---
+++ src/test/java/org/example/zoopark/ZooparkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zoopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
