7403cc088ce04f4ead9b197c5aef82463f5448e4
Akybek Abdikulov <akybek.abdikulov@narxoz.kz>
2025-12-20 17:04:40 +0500

    I have created security.config and add class usermodel and permisions

---
+++ src/main/java/org/example/zoopark/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.zoopark.repository;
+
+import org.example.zoopark.entity.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByEmail(String username);
+}
